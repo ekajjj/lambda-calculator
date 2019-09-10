06c0b384134e402e02d9a5effbba275e6634253d
Jake Johnson <jakejjohnson@gmail.com>
2019-09-10 17:10:19 -0500

    seems to function after debugging, need to style yet

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
 
-const Numbers = () => {
+//Import your array data to from the provided data file
+import {numbers} from '../../../data.js'
+import NumberButton from "./NumberButton.js";
+
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  const [numberState] = useState(numbers)
   return (
-    <div>
+    <div className='numbersContainer'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numberState.map((number, indx) => {
+         return indx !== 9 
+         ? <NumberButton 
+              key={indx} 
+              symbol={number}
+              setDisplayResult={props.setDisplayResult}
+              displayResult={props.displayResult} /> 
+         : <NumberButton 
+              key={indx} 
+              symbol={number} 
+              name='zeroNum' 
+              setDisplayResult={props.setDisplayResult}
+              displayResult={props.displayResult} /> 
+       })}
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import SpecialButton from './SpecialButton'
 
 //Import your array data to from the provided data file
+import {specials} from '../../../data.js'
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
+  const [specialsState] = useState(specials)
 
   return (
-    <div>
+    <div className='specialsContainer'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specialsState.map((special, indx) => {
+         return <SpecialButton 
+                  key={indx} 
+                  symbol={special}
+                  displayResult={props.displayResult} 
+                  setDisplayResult={props.setDisplayResult}/>
+       })}
     </div>
   );
 };
+
+export default Specials;
